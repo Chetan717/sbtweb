6f4f647daac53bbfef1589b8619d47328d119d20
chaitanya chaudhari <118418197+Chetan717@users.noreply.github.com>
2025-02-07 20:47:00 +0530

    Update Data.js

--- src/app/Offer/Data.js
+++ src/app/Offer/Data.js
@@ -2,20 +2,20 @@
 const DataOffer = [
   {
     title: "Best Deal",
-    discount: "50%",
+    discount: "5% To 18%",
     sub: "Save upto",
     imageURL: "/ban1.png",
   },
   {
     title: "Limited  Offer",
-    discount: "40%",
+    discount: "5% To 10%",
     sub: "Save upto",
     imageURL: "/ban2.png",
   },
 
   {
     title: "Best Price",
-    discount: "₹200",
+    discount: "5% To 16%",
     sub: "Starting",
     imageURL: "/ban3.png",
   },
