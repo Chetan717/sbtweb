93e48e7153f22a56e8fe5ddbb70bbf9be083eb96
chaitanya chaudhari <118418197+Chetan717@users.noreply.github.com>
2025-02-09 15:44:20 +0530

    Update PopularProd.jsx

--- src/app/PopularProd/PopularProd.jsx
+++ src/app/PopularProd/PopularProd.jsx
@@ -1,9 +1,10 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import PopularProdList from "./Comp/PopularProdList";
 import { useDispatch } from "react-redux";
 import { ProTypeRed } from "../Redux/Slice/ProductTypeSlice";
 import { setLastIndex } from "../Redux/Slice/LoadMore";
+
 export default function PopularProd() {
   const dispatch = useDispatch();
   const img = [
@@ -16,42 +17,41 @@ export default function PopularProd() {
     { value: "cow", name: "Cow Feeds" },
   ];
   const [selType, setSelType] = React.useState("organic");
-  const SelectType = (selType) => {
-    dispatch(ProTypeRed(selType));
+
+  useEffect(() => {
+    handleSelectType("organic");
+  }, []);
+
+  const handleSelectType = (selectedType) => {
+    dispatch(ProTypeRed(selectedType));
     dispatch(setLastIndex(8));
-    setSelType(selType);
+    setSelType(selectedType);
   };
 
   return (
-    <>
-      <main className="flex flex-col bg-white justify-center  items-center  gap-7  ">
-        <h1 className="text- font-bold text-xl"> Popular Products</h1>
-        <div
-          id="prod"
-          className="flex lg:flex-row flex-col gap-[15px]  justify-center items-center "
-        >
-          <div className="flex mt-3 flex-wrap gap-4 lg:gap-10 justify-center items-center">
-            {img?.map((i) => {
-              return (
-                <>
-                  <p
-                    key={i}
-                    onClick={() => SelectType(i.value)}
-                    className={
-                      selType === i.value
-                        ? `font-semibold text-xs lg:text-sm  bg-teal-500 rounded-lg p-2 text-white  hover:-translate-y-1 cursor-pointer`
-                        : `font-semibold text-xs bg-gray-50 rounded-lg p-2 text-gray-800  hover:-translate-y-1 cursor-pointer`
-                    }
-                  >
-                    {i.name}
-                  </p>
-                </>
-              );
-            })}
-          </div>
+    <main className="flex flex-col bg-white justify-center items-center gap-7">
+      <h1 className="font-bold text-xl">Popular Products</h1>
+      <div
+        id="prod"
+        className="flex lg:flex-row flex-col gap-[15px] justify-center items-center"
+      >
+        <div className="flex mt-3 flex-wrap gap-4 lg:gap-10 justify-center items-center">
+          {img.map((item) => (
+            <p
+              key={item.value}
+              onClick={() => handleSelectType(item.value)}
+              className={
+                selType === item.value
+                  ? "font-semibold text-xs lg:text-sm bg-teal-500 rounded-lg p-2 text-white hover:-translate-y-1 cursor-pointer"
+                  : "font-semibold text-xs bg-gray-50 rounded-lg p-2 text-gray-800 hover:-translate-y-1 cursor-pointer"
+              }
+            >
+              {item.name}
+            </p>
+          ))}
         </div>
-        <PopularProdList selType={selType} />
-      </main>
-    </>
+      </div>
+      <PopularProdList selType={selType} />
+    </main>
   );
 }
